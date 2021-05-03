c01d96631f8425fbfaf704b315f9ff7c05a75ca3
ZakiaGh <zakiaghouli8@gmail.com>
2021-05-03 12:45:23 +0200

    projet 1

---
+++ le_juste_prix.py
@@ -0,0 +1,18 @@
+import random
+
+n = random.randint(0,100)
+commentaire = "?"
+while True:
+    var = input("Entrez un nombre")
+    var = int(var)
+    if var < n :
+        commentaire = "trop bas"
+        print(var, commentaire)
+
+    else :
+        commentaire = "trop haut"
+        print(var, commentaire)
+    if var == n:
+        commentaire = "bravo !"
+        print(var, commentaire)
+        break
