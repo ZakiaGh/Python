5f6476b2631e439d6428a268003cc8af27e4140c
ZakiaGh <zakiaghouli8@gmail.com>
2021-05-03 16:18:38 +0200

    commit

--- PierrePapierCiseaux.py
+++ PierrePapierCiseaux.py
@@ -11,7 +11,7 @@ while input("Jouez (y/n): ").lower() != "n":
     a = int(input("Choisissez un chiffre :\n0: Pierre\n1: Feuille\n2: Ciseaux\n-> "))
     b = choice(range(3))
  
-    print("\n{} VS {}".format(coup[a], coup[b]))
+    print("\n{} || {}".format(coup[a], coup[b]))
     if a == b:
         print("ÉGALITÉ\n")
     elif (a>b and b+1 == a) or (a<b and a+b == 2):
--- le_juste_prix.py
+++ le_juste_prix.py
@@ -24,16 +24,15 @@
 from random import randint
 
 nbr_essais_max = 6
-nbr_essais = 1
-borne_sup = 100
-mon_prix = randint(1,borne_sup)
+essais = 1
+borneSup = 100
+mon_prix = randint(1,borneSup)
 ton_prix = 0
 
-print("J'ai choisi un prix entre 1 et",borne_sup)
-print("A vous de le deviner en",nbr_essais_max,"tentatives au maximum !")
-
-while ton_prix != mon_prix and nbr_essais <= nbr_essais_max:
-    print("Essai numero : ",nbr_essais)    
+print("J'ai choisi un prix entre 1 et",borneSup)
+print("Vous avez",essais_max,"tentatives !")
+while ton_prix != mon_prix and nbr_essais <= essais_max:
+    print("Essai numero : ",essais)    
     ton_prix = int(input("Votre proposition : "))
     if ton_prix < mon_prix:
         print("Trop petit")
@@ -43,6 +42,6 @@ while ton_prix != mon_prix and nbr_essais <= nbr_essais_max:
         print("Bravo ! Vous avez trouvé",mon_prix,"en",nbr_essais,"essai(s)")   
     nbr_essais += 1
     
-if nbr_essais>nbr_essais_max and ton_prix != mon_prix :
-    print("Désolé, vous avez utilisé vos",nbr_essais_max,"essais.")    
+if essais>essais_max and ton_prix != mon_prix :
+    print("Désolé, vous avez utilisé vos",essais_max,"essais.")    
     print("J'avais choisi le prix",mon_prix,".")
